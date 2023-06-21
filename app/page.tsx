8ad18217cd42f80f87c9d084abb13201d88bb1bc
Nikita Sherbakov <nikita@sherbakov.dev>
2023-06-21 10:41:31 +0300

    feat: :tada: Init commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,148 @@
-import Image from 'next/image'
+"use client";
+
+import toast, { Toaster } from "react-hot-toast";
+
+import { ec } from "elliptic";
+import schema from "./InscriptionManifest.schema.json";
+import schemaContent from "./InscriptionManifest.content.schema.json";
+import { sha256 } from "js-sha256";
+import { useOrdinalSafe } from "./useOrdinalSafe";
+import { validate } from "jsonschema";
+
+// Take from the collection manifest
+const collection = {
+  paymentAddress:
+    "bc1ptg082sqegddsht6wzuxh5akwdd0ydtprqrw6u5nu0ytlruwkmghsf2nlkt",
+  signerPublicKey:
+    "0399764fc2dd350bc7fb6acedbd06160a79779aaddc1793a526e8d2fb386b5f287",
+};
+
+const token = {
+  protocol: { name: "BRC721", version: "0.4.0" },
+  type: "inscription",
+  content:
+    '{"collectionInscriptionId":"8abc170f6ae5d300264e245dedd9faf7f1ba7da9029e05b08344ac5368d8d076i0","price":75000}',
+  contentSignature:
+    "3044022041f99e96d43d97a9ed9661770d73df229e5fb7a1d36f8d2878c72db36a4f147b022045bdd29db1436418d5ef081f60d89948cf08b7209cebf7e02381697a0bee5a49",
+};
+
+const inscriptionJSON = JSON.stringify(token, null, 0);
 
 export default function Home() {
+  const wallet = useOrdinalSafe();
+
+  const inscribe = async () => {
+    let inscription;
+    let inscriptionContent;
+
+    try {
+      inscription = JSON.parse(inscriptionJSON);
+    } catch {
+      toast.error("Invalid inscription JSON");
+      return;
+    }
+
+    try {
+      inscriptionContent = JSON.parse(inscription.content);
+    } catch {
+      toast.error("Invalid inscription content JSON");
+      return;
+    }
+
+    if (!wallet.wallet) {
+      toast.error("No wallet connected");
+      return;
+    }
+
+    const [userAddress] = await wallet.wallet.requestAccounts();
+
+    if (!validate(inscription, schema).valid) {
+      toast.error("Invalid inscription manifest");
+      return;
+    }
+
+    if (!validate(inscriptionContent, schemaContent).valid) {
+      toast.error("Invalid inscription manifest content");
+      return;
+    }
+
+    if (inscriptionContent.initialOwnerAddress) {
+      if (inscriptionContent.initialOwnerAddress !== userAddress) {
+        toast.error("Not allowed to inscribe for another user");
+        return;
+      }
+    }
+
+    // Verify signature
+    const secp256k1 = new ec("secp256k1");
+    const uncompressed = collection.signerPublicKey.length === 128;
+    const signature = inscription.contentSignature;
+    const verifyingKey = secp256k1.keyFromPublic(
+      uncompressed
+        ? `04${collection.signerPublicKey}`
+        : collection.signerPublicKey,
+      "hex"
+    );
+    const messageHash = sha256(Buffer.from(inscription.content, "utf8"));
+
+    try {
+      if (!verifyingKey.verify(messageHash, signature)) {
+        toast.error("Bad signature");
+        return;
+      }
+    } catch {
+      toast.error("Bad signature");
+      return;
+    }
+
+    wallet.inscribe(inscriptionJSON, [
+      { fee: inscriptionContent.price, receiver: collection.paymentAddress },
+    ]);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      {(() => {
+        if (wallet.injection.isError) {
+          return (
+            <div>
+              {wallet.injection.error}.{" "}
+              <a
+                target="_blank"
+                className="link"
+                href="https://ordinalsafe.xyz"
+              >
+                Download from the official page.
+              </a>
+            </div>
+          );
+        }
+
+        if (wallet.initialization.isError) {
+          return <div>{wallet.initialization.error}</div>;
+        }
+
+        if (wallet.injection.isSuccess && wallet.initialization.isIdle) {
+          return <button onClick={wallet.initialize}>Connect wallet</button>;
+        }
+
+        if (wallet.initialization.isSuccess) {
+          return <button onClick={inscribe}>Inscribe</button>;
+        }
+      })()}
+
+      {wallet.inscriptionManifest.data?.reveal ? (
+        <div className="mt-4">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
-            rel="noopener noreferrer"
+            className="text-white underline hover:no-underline"
+            href={`https://www.blockchain.com/explorer/transactions/btc/${wallet.inscriptionManifest.data.reveal}`}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
+            {wallet.inscriptionManifest.data.reveal}
           </a>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      ) : null}
+      <Toaster />
     </main>
-  )
+  );
 }
